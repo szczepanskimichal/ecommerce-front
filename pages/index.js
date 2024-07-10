ada686408876e8edf63ef6633e87916d9de67c22
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-07-10 21:52:38 +0200

    Create pages(index.js
    Add Header component to HomePage
    
    - Imported Header component from "@/components/Header".
    - Integrated Header component into HomePage.
    - Structured HomePage with a flex column layout and full-screen height for the header section.
    
    This enhancement includes the navigation header in the home page layout, providing a consistent user experience across the application.

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Header from "@/comonents/Header";
+
+export default function HomePage() {
+  return (
+    <div className="flex flex-col">
+      <div className="h-screen flex flex-col">
+        <Header />
+      </div>
+    </div>
+  );
+}
