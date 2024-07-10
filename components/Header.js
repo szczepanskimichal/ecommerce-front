b5c6612ff81a375770142b3a51ab330cf2a10f90
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-07-10 22:37:56 +0200

    Update components/Header.js
    Enhance Header component with active link styles
    
    - Added styles for active and inactive links.
    - Integrated Next.js router to determine the current path.
    - Applied conditional styling to highlight the active link based on the current path.
    
    This update improves navigation by visually indicating the active page.

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,38 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+
 export default function Header() {
+  const inactiveLink =
+    "hover:text-primary hover:scale-105 hover:decoration-primary decoration-secondary underline underline-offset-4 transition-all delay-150 duration-300";
+  const activeLink = inactiveLink.replace(
+    "decoration-secondary",
+    "decoration-white"
+  );
+
+  const routes = useRouter();
+  const { pathname } = routes;
+
   return (
     <>
       <header className="fixed top-0 w-full hidden sm:flex justify-around h-[80px] items-center bg-secondary text-white z-2">
-        <Link href="/">Ecommerce</Link>
+        <Link className={inactiveLink} href="/">
+          Ecommerce
+        </Link>
         <nav className="flex gap-10">
-          <Link href={"/"}>Home</Link>
-          <Link href={"/products"}>All Products</Link>
+          <Link
+            className={`${pathname == "/" ? activeLink : inactiveLink}`}
+            href={"/"}
+          >
+            Home
+          </Link>
+          <Link
+            className={`${
+              pathname.includes("/products") ? activeLink : inactiveLink
+            }`}
+            href={"/products"}
+          >
+            All Products
+          </Link>
           <div>
             <div>Categories</div>
           </div>
