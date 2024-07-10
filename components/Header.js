18643ad04f93f4edd18b2a38dd5b56afcae462cf
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-07-11 00:50:57 +0200

    Update Header.js
    I added new links to the burger along with the styles, and at the very bottom i added an svg from the account
    
    Enhance mobile view header with navigation and animated menu
    
    - Added mobile-specific header with navigation links and menu icon.
    - Implemented state management to toggle navigation menu visibility.
    - Utilized framer-motion for animated menu transitions (slide-in from left).
    - Improved user experience with responsive design and smooth animations.
    
    This commit integrates the mobile view header enhancements to improve navigation and usability on smaller screens.

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { slideIn } from "@/utils/motion";
 
 export default function Header() {
   const inactiveLink =
@@ -12,18 +14,17 @@ export default function Header() {
 
   const [navOpen, setNavOpen] = useState(false);
 
-  const routes = useRouter();
-  const { pathname } = routes;
-
+  const router = useRouter();
+  const { pathname } = router;
   return (
     <>
       <header className="fixed top-0 w-full hidden sm:flex justify-around h-[80px] items-center bg-secondary text-white z-2">
-        <Link className={inactiveLink} href="/">
+        <Link className={inactiveLink} href={"/"}>
           Ecommerce
         </Link>
         <nav className="flex gap-10">
           <Link
-            className={`${pathname == "/" ? activeLink : inactiveLink}`}
+            className={`${pathname === "/" ? activeLink : inactiveLink}`}
             href={"/"}
           >
             Home
@@ -40,7 +41,6 @@ export default function Header() {
             <div>Categories</div>
           </div>
         </nav>
-        {/* 2 element nawigacji */}
         <nav className="flex gap-10 items-center">
           <Link
             href={"/account/profile"}
@@ -86,9 +86,10 @@ export default function Header() {
           </Link>
         </nav>
       </header>
-      {/* widok mobilny */}
       <header
-        className={`w-full sm:hidden flex justify-around h-[70px] items-center transition delay-50 duration-500`}
+        className={`w-full sm:hidden flex justify-around h-[70px] items-center transition delay-50 duration-500 text-white ${
+          navOpen ? "bg-black" : "bg-secondary"
+        }`}
       >
         <div onClick={() => setNavOpen(true)}>
           <svg
@@ -104,6 +105,89 @@ export default function Header() {
             />
           </svg>
         </div>
+        <Link href={"/"}>Ecommerce</Link>
+        <Link href={"/cart"} className="group">
+          <div className="flex items-center h-[60px] relative transition delay-150 duration-300 group-hover:text-primary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-7"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+              />
+            </svg>
+            <div className="absolute top-2 left-4 bg-secondary text-white border-2 border-white rounded-full items-center justify-center flex size-5 text-xs transition delay-150 duration-300 group-hover:text-primary group-hover:border-primary">
+              0
+            </div>
+          </div>
+        </Link>
+        <AnimatePresence>
+          {navOpen && (
+            <motion.nav
+              variants={slideIn("left", "tween", 0, 0.5, false)}
+              initial="hidden"
+              whileInView="show"
+              exit="exit"
+              className="absolute top-0 left-0 h-screen bg-secondary w-[60%] pl-[60px]"
+            >
+              <div
+                onClick={() => setNavOpen(false)}
+                className="absolute top-5 right-5"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="size-7"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="flex flex-col justify-between h-[40%]">
+                <nav className="flex flex-col gap-10 mt-[100px] justify-center">
+                  <Link className={inactiveLink} href={"/"}>
+                    Home
+                  </Link>
+                  <Link className={inactiveLink} href={"/"}>
+                    All Products
+                  </Link>
+                  <Link className={inactiveLink} href={"/"}>
+                    Categories
+                  </Link>
+                </nav>
+                <nav className="flex flex-col gap-10 justify-center">
+                  <Link href={"/account"} className="flex gap-3 items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-9"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                      />
+                    </svg>
+                    Account
+                  </Link>
+                </nav>
+              </div>
+            </motion.nav>
+          )}
+        </AnimatePresence>
       </header>
     </>
   );
