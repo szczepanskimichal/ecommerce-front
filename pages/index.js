e504a991987c2e7f56a38b0e865589e85d3adcff
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-07-17 12:02:50 +0200

    Update Index.js
    Enhance HomePage with new product and category listings
    
    - Added NewProducts component to HomePage to display the latest products.
    - Updated Header component to receive and display categories.
    - Enhanced getServerSideProps to fetch and provide new products and categories to HomePage.
    - Modified Featured component integration to remain consistent.
    - Improved data fetching logic to include new products and categories alongside the featured product.
    - Updated the layout to include NewProducts section below the featured product.

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
-import Header from "@/components/Header";
 import Featured from "@/components/Featured";
+import Header from "@/components/Header";
+import NewProducts from "@/components/NewProducts";
 import { mongooseConnect } from "@/lib/mongoose";
+import { Category } from "@/models/Category";
 import { Product } from "@/models/Product";
 
-export default function HomePage({ featuredProduct }) {
-  // Zmieniono 'featuredProducts' na 'featuredProduct'
+export default function HomePage({ featuredProduct, newProducts, categories }) {
   return (
     <div className="flex flex-col">
       <div className="h-screen flex flex-col">
-        <Header />
-        <Featured product={featuredProduct} />{" "}
+        <Header categories={categories} />
+        <Featured product={featuredProduct} />
       </div>
+      <NewProducts products={newProducts} />
     </div>
   );
 }
@@ -19,9 +21,16 @@ export async function getServerSideProps() {
   const featuredProductId = "66964e512c84a13a661c4eb6";
   await mongooseConnect();
   const featuredProduct = await Product.findById(featuredProductId);
+  const newProducts = await Product.find({}, null, {
+    sort: { _id: -1 },
+    limit: 10,
+  });
+  const categories = await Category.find({}, null, { sort: { _id: -1 } });
   return {
     props: {
       featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
+      newProducts: JSON.parse(JSON.stringify(newProducts)),
+      categories: JSON.parse(JSON.stringify(categories)),
     },
   };
 }
