42082e9e0e891b04090f1e96263d26310535a660
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-07-17 14:59:32 +0200

    Create components/Layout.js
    Create Layout component with header and responsive design
    
    - Introduced Layout component to provide a consistent structure across pages.
    - Included Header component within Layout for top navigation.
    - Added state management for categories using useState and useEffect hooks.
    - Placeholder function for fetching categories via API (commented out for now).
    - Implemented responsive design with flexbox to ensure proper layout across different screen sizes.
    - Ensured minimum height to cover the viewport and added padding for small screens.

---
+++ components/Layout.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+import Header from "./Header";
+
+export default function Layout({ children }) {
+  const [categories, setCategories] = useState([]);
+
+  // function fetchCategories() {
+  //     axios.get("/api/categories")
+  // }
+  return (
+    <div className="flex flex-col min-h-[100vh] bg-gray-300">
+      <Header />
+      <div className="min-h-screen sm:pt-[80px] flex justify-center">
+        <div className="w-full">{children}</div>
+      </div>
+    </div>
+  );
+}
