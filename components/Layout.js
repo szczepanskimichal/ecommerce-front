f87000d04f197161b3493fc4a1649eea45e89eb6
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-07-22 15:06:17 +0200

    Update Layout.js
    Add categories fetching in Layout component
    
    - Import axios for HTTP requests in Layout component.
    - Add useEffect to fetch categories on component mount.
    - Update fetchCategories function to set state with fetched data.
    - Pass fetched categories to the Header component.

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,16 @@
 import { useState, useEffect } from "react";
 import Header from "./Header";
-
+import axios from "axios";
 export default function Layout({ children }) {
   const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetchCategories();
+  }, []);
 
   function fetchCategories() {
-    axios.get("/api/categories");
+    axios.get("/api/categories").then((response) => {
+      setCategories(response.data);
+    });
   }
   return (
     <div className="flex flex-col min-h-[100vh] bg-gray-300">
