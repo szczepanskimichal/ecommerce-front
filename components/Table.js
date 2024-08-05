23eb681ad90054ab09fba8d23db5170d71425a5e
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-08-05 15:53:49 +0200

    Update Table.js
    Enhance product table to display product images, titles, and enable cart actions
    
    - Added product image and title display in the table.
    - Implemented removeProduct and addProduct buttons for cart management.
    - Displayed the price of each product.
    - Included a clear cart button with functionality.
    - Calculated and displayed the total price at the bottom of the table.

--- components/Table.js
+++ components/Table.js
@@ -24,10 +24,46 @@ export default function Table({ products, cartProducts }) {
         {products.map((product) => (
           <tr key={product._id} className="border-t-2">
             <td className="h-[140px] sm:h-[150px] mb-2 rounded-lg flex flex-col justify-center gap-1">
-              <Link href={"/products/" + product._id}></Link>
+              <Link href={"/products/" + product._id}>
+                <img
+                  src={product.images[0]}
+                  className="bg-white max-h-[80px] sm:max-w-[100px] sm:max-h-[100px] border rounded-lg p-2"
+                />
+              </Link>
+              {product.title}
             </td>
+            <td>
+              <div className="flex items-center gap-3">
+                <button
+                  onClick={() => removeProduct(product._id)}
+                  className="bg-gray-200 hover:bg-gray-300 py-1"
+                >
+                  -
+                </button>
+                {cartProducts.filter((id) => id === product._id).length}
+                <button
+                  onClick={() => addProduct(product._id)}
+                  className="bg-gray-200 hover:bg-gray-300 py-1"
+                >
+                  +
+                </button>
+              </div>
+            </td>
+            <td>${product.price}</td>
           </tr>
         ))}
+        <tr className="sticky bottom-0 bg-gray-100 border-t-2 border-gray-400 font-semibold">
+          <td>
+            <div
+              onClick={() => clearCart()}
+              className="p-0 hover:text-gray-500 cursor-pointer inline-flex"
+            >
+              Clear Cart
+            </div>
+          </td>
+          <td className="flex justify-end">Total:</td>
+          <td>${total}</td>
+        </tr>
       </tbody>
     </table>
   );
