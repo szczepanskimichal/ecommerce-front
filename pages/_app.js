afb0344342bf3b3572a5f4b7f416cf6dc1fcd9c0
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-08-05 11:49:30 +0200

    Update pages/_app.js
    Add toast notifications with react-hot-toast
    
    - Imported Toaster component from "react-hot-toast".
    - Added Toaster configuration in the main App component.
    - Configured toast notifications to appear at the top-center with specific styles and options.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,26 @@
 import { CartContextProvider } from "@/components/CartContext";
 import "@/styles/globals.css";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }) {
   return (
     <CartContextProvider>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        gutter={8}
+        toastOptions={{
+          duration: 3000,
+          style: {
+            fontSize: "20px",
+            padding: "20px",
+            display: "inline-flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            whiteSpace: "nowrap",
+          },
+        }}
+      />
       <Component {...pageProps} />;
     </CartContextProvider>
   );
