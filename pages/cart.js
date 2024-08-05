68deec9351f544df80aacc23706e22a534252552
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-08-05 17:03:11 +0200

    Update pages/cart.js
    Refactor CartPage component with motion effects and added UserForm component
    
    - Imported motion from framer-motion and fadeIn from utils/motion
    - Added animation effects to cart and order information sections using motion.div
    - Removed unused clearCart function from CartContext
    - Corrected 'hileInView' typo to 'whileInView'
    - Corrected 'useForm' to 'UserForm' and ensured it is imported
    - Implemented isMobile check for conditional animations

--- pages/cart.js
+++ pages/cart.js
@@ -3,6 +3,8 @@ import Layout from "@/components/Layout";
 import Table from "@/components/Table";
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "@/utils/motion";
 
 export default function CartPage() {
   const { cartProducts } = useContext(CartContext);
@@ -22,16 +24,31 @@ export default function CartPage() {
     <Layout>
       <div className="flex justify-center p-5 sm:p-10"></div>
       <div className="sm:min-w-[40rem] sm:w-[80%] flex flex-col sm:grid grid-cols-3 gap-10 ">
-        <div className="box p-5 col-span-1 text-sm sm:text-normal">
+        <motion.div
+          variants={fadeIn("right", "spring", 0.1, 1)}
+          initial="hidden"
+          hileInView="show"
+          className="box p-5 col-span-2 text-sm sm:text-normal"
+        >
           {!cartProducts?.length && <h3>Your cart is empty</h3>}
           {products?.length > 0 && (
-            <div>
+            <>
               <h3>Cart</h3>
               <Table products={products} cartProducts={cartProducts} />
-            </div>
+            </>
           )}
-        </div>
-        <div className="box p-5"></div>
+        </motion.div>
+        <motion.div
+          variants={fadeIn(isMobile ? "down" : "left", "spring", 0.1, 1)}
+          initial="hidden"
+          whileInView="show"
+          className="box p-5 sticky top-[120px]"
+        >
+          <h3>Order information</h3>
+          <div>
+            <useForm cartProducts={cartProducts} />
+          </div>
+        </motion.div>
       </div>
     </Layout>
   );
