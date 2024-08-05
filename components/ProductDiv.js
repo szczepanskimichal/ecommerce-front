77de3fd42a1995af735d925d547c8ab5a309ddfd
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-08-05 12:29:22 +0200

    Update ProductDiv.js
    Refactored ProductDiv component to use CartContext and animate add to cart button
    
    - Added useContext to import CartContext and use the addProduct function.
    - Implemented handleAddToCart function to trigger addProduct and animate the button.
    - Replaced SVG icon with CartIcon component for better modularity.
    - Added animation logic to handleAddToCart to add and remove "animate" class from the button.
    - Updated HTML structure and class names for consistency.
    
    This update ensures a smoother user experience by providing visual feedback when an item is added to the cart.

--- components/ProductDiv.js
+++ components/ProductDiv.js
@@ -3,11 +3,19 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import { useContext } from "react";
 import { CartContext } from "./CartContext";
+import CartIcon from "./CartIcon";
 
 export default function ProductDiv({ _id, title, images, price, index }) {
   const { addProduct } = useContext(CartContext);
   function handleAddToCart() {
     addProduct(_id);
+    const button = document
+      .getElementsByClassName(index)
+      .querySelector(".btn-outline");
+    button.classList.add("animate");
+    setTimeout(() => {
+      button.classList.remove("animate");
+    }, 1000);
   }
 
   return (
@@ -31,20 +39,7 @@ export default function ProductDiv({ _id, title, images, price, index }) {
         <div className="flex gap-3 justify-between items-center mt-3">
           <p className="text-2xl font-bold">${price}</p>
           <button className="btn-outline" onClick={handleAddToCart}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-7"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-              />
-            </svg>
+            <CartIcon className="size-7" />
             Add to Cart
           </button>
         </div>
