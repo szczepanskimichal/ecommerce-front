b51b7100d4aca7bdf9b1ff1a1068207de00be5c2
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-08-05 15:54:44 +0200

    Update Cart.js
    Simplify Product find query in API endpoint
    
    - Removed unnecessary $in operator from Product.find query.
    - Updated the query to directly match _id with ids.

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -3,6 +3,7 @@ import { Product } from "@/models/Product";
 
 export default async function handle(req, res) {
   await mongooseConnect();
+
   const ids = req.body.ids;
-  res.json(await Product.find({ _id: { $in: ids } }));
+  res.json(await Product.find({ _id: ids }));
 }
