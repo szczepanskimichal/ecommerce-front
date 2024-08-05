637fe7f9ca5a08de02e91271f653326661991400
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-08-05 18:36:01 +0200

    Update global.css
    Add styling for form elements using Tailwind CSS
    
    - Applied border, padding, and rounded corners to input, textarea, and select elements.
    - Added focus states for improved accessibility and user experience.
    - Included margin-bottom for spacing and shadow for better visibility.

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 import Header from "./Header";
 import axios from "axios";
+
 export default function Layout({ children }) {
   const [categories, setCategories] = useState([]);
+
   useEffect(() => {
     fetchCategories();
   }, []);
@@ -14,7 +16,7 @@ export default function Layout({ children }) {
   }
   return (
     <div className="flex flex-col min-h-[100vh] bg-gray-300">
-      <Header />
+      <Header categories={categories} />
       <div className="min-h-screen sm:pt-[80px] flex justify-center">
         <div className="w-full">{children}</div>
       </div>
