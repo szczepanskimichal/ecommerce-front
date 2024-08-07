de970af94061aeac4cafdc7a142260525a5921c9
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-08-07 13:06:32 +0200

    Update motion utility functions
    - Added `dropIn` variant for smooth entrance and exit animations with spring effect.
    - Added `navVariants` for navigation animations with spring transition.
    - Modified `slideIn` to include optional opacity parameter for better control of visibility during transitions.
    - Enhanced `fadeIn` with additional damping and stiffness properties for more refined animation control.

--- utils/motion.js
+++ utils/motion.js
@@ -1,8 +1,50 @@
-export const slideIn = (direction, type, delay, duration) => ({
+export const dropIn = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 1,
+      type: "spring",
+      damping: 20,
+      stiffness: 300,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+  },
+};
+
+export const navVariants = {
+  hidden: {
+    opacity: 0,
+    y: -50,
+    transition: {
+      type: "spring",
+      stiffness: 300,
+      damping: 140,
+    },
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 80,
+      delay: 1,
+    },
+  },
+};
+
+export const slideIn = (direction, type, delay, duration, opacity) => ({
   hidden: {
     x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
     y: direction === "up" ? "100%" : direction === "down" ? "-100%" : 0,
-    opacity: 0,
+    opacity: opacity ? 0 : 1,
   },
   show: {
     x: 0,
@@ -17,8 +59,8 @@ export const slideIn = (direction, type, delay, duration) => ({
   },
   exit: {
     x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
-    y: direction === "up" ? "100%" : direction === "down" ? "100%" : 0,
-    opacity: 0,
+    y: direction === "up" ? "100%" : direction === "down" ? "-100%" : 0,
+    opacity: opacity ? 0 : 1,
   },
 });
 
@@ -37,26 +79,13 @@ export const fadeIn = (direction, type, delay, duration) => ({
       delay,
       duration,
       ease: "easeOut",
+      damping: 30,
+      stiffness: 300,
     },
   },
   exit: {
     x: direction === "left" ? -50 : direction === "right" ? 50 : 0,
     y: direction === "up" ? -50 : direction === "down" ? 50 : 0,
     opacity: 0,
-  },
-});
-
-export const cardVariants = (direction, type, delay, duration) => ({
-  hidden: { opacity: 0, x: direction === "left" ? -100 : 100 },
-  show: {
-    opacity: 1,
-    x: 0,
-    y: 0,
-    transition: {
-      type,
-      delay,
-      duration,
-      ease: "easeOut",
-    },
   },
 });
