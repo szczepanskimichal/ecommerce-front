02cd8f3633e695a592306b79251a11e945674484
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-08-07 14:10:23 +0200

    Wrap App component with SessionProvider for session management

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,30 @@
 import { CartContextProvider } from "@/components/CartContext";
 import "@/styles/globals.css";
 import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }) {
   return (
-    <CartContextProvider>
-      <Toaster
-        position="top-center"
-        reverseOrder={false}
-        gutter={8}
-        toastOptions={{
-          duration: 3000,
-          style: {
-            fontSize: "20px",
-            padding: "8px 16px",
-            display: "inline-flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            whiteSpace: "nowrap",
-          },
-        }}
-      />
-      <Component {...pageProps} />
-    </CartContextProvider>
+    <SessionProvider>
+      <CartContextProvider>
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          gutter={8}
+          toastOptions={{
+            duration: 3000,
+            style: {
+              fontSize: "20px",
+              padding: "8px 16px",
+              display: "inline-flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              whiteSpace: "nowrap",
+            },
+          }}
+        />
+        <Component {...pageProps} />
+      </CartContextProvider>
+    </SessionProvider>
   );
 }
