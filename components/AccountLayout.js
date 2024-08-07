81aca494125bc5a3b75c4c29b5c3bab5d2b06b2f
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-08-07 12:39:46 +0200

    Update AccountLayout component
    - Added AccountLink component for profile navigation.
    - Imported ProfileIcon and integrated it into the AccountLink.
    - Updated sidebar to include a navigational link for profile information.
    - Ensured layout and styling are consistent with previous design.

--- components/AccountLayout.js
+++ components/AccountLayout.js
@@ -1,3 +1,5 @@
+import AccountLink from "./AccountLink";
+import ProfileIcon from "./icons/ProfileIcon";
 import Layout from "./Layout";
 
 export default function AccountLayout() {
@@ -5,7 +7,13 @@ export default function AccountLayout() {
     <Layout>
       <div className="p-5 w-full h-full">
         <div className="box w-full h-full flex flex-col sm:flex-row flex-grow">
-          <aside className="h-full rounded-t-lg sm:rounded-l-lg sm:rounded-r-none bg-gray-200 flex justify-center sm:justify-start sm:flex-col gap-3 p-5 whitespace-nowrap"></aside>
+          <aside className="h-full rounded-t-lg sm:rounded-l-lg sm:rounded-r-none bg-gray-200 flex justify-center sm:justify-start sm:flex-col gap-3 p-5 whitespace-nowrap">
+            <AccountLink
+              href="profile"
+              title="Profile information"
+              Icon={ProfileIcon}
+            />
+          </aside>
         </div>
       </div>
     </Layout>
