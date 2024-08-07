ada767c1647d267b5caadfba7efadec5cf9a0ca3
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-08-07 14:25:19 +0200

    Add FormInput component with basic input handling
    - Created FormInput component to handle various input types.
    - Added label and placeholder based on props.
    - Included disabled state to handle user creation process.
    - Prepared for error handling with commented-out AnimatePresence and ErrorMessage components.
    - Managed input state through value and onChange props.

---
+++ components/FormInput.js
@@ -0,0 +1,30 @@
+import { AnimatePresence } from "framer-motion";
+
+export default function FormInput({
+  label,
+  type,
+  value,
+  onChange,
+  creatingUser,
+  error,
+}) {
+  return (
+    <>
+      <div className="flex justify-between">
+        <label>{label}</label>
+        {/* <AnimatePresence>
+                    {error !== null && (
+                        <ErrorMessage />
+                    )}
+                </AnimatePresence> */}
+      </div>
+      <input
+        type={type}
+        placeholder={label}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        disabled={creatingUser}
+      />
+    </>
+  );
+}
