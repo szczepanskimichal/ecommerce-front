823c9ad694fcc3a1ed5814c259d224d6f0a4ffc8
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2024-08-07 10:42:22 +0200

    Create pages/account/AccountLayout
    Add AccountLayout component:
    
    - Created AccountLayout component to provide a consistent layout for account-related pages.
    - Included a sidebar structure with responsive design using Tailwind CSS classes.
    - Utilized Layout component to wrap the AccountLayout for overall page layout consistency.

---
+++ components/AccountLayout.js
@@ -0,0 +1,13 @@
+import Layout from "./Layout";
+
+export default function AccountLayout() {
+  return (
+    <Layout>
+      <div className="p-5 w-full h-full">
+        <div className="box w-full h-full flex flex-col sm:flex-row flex-grow">
+          <aside className="h-full rounded-t-lg sm:rounded-l-lg sm:rounded-r-none bg-gray-200 flex justify-center sm:justify-start sm:flex-col gap-3 p-5 whitespace-nowrap"></aside>
+        </div>
+      </div>
+    </Layout>
+  );
+}
